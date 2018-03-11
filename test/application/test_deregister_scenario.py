eab8de4966944823ff6a4804a14f171e0f37ac92
Thomas Reifenberger <tom-mi@users.noreply.github.com>
2018-03-11 21:29:47 +0100

    Add registration test

--- test/application/test_deregister_scenario.py
+++ test/application/test_deregister_scenario.py
@@ -4,6 +4,23 @@ from async2v.components.base import BareComponent, EventDrivenComponent
 from async2v.fields import Output, Buffer
 
 
+def test_register_during_runtime(app):
+    source = SampleSource()
+    sink1 = SampleSink()
+    sink2 = SampleSink()
+    app.register(source, sink1)
+    app.start()
+    time.sleep(0.1)
+    source.push('1')
+    app.register(sink2)
+    source.push('2')
+    time.sleep(0.2)
+    app.stop()
+
+    assert sink1.log == ['setup', '1', '2', 'cleanup']
+    assert sink2.log == ['setup', '2', 'cleanup']
+
+
 def test_deregister_during_runtime(app):
     source = SampleSource()
     sink1 = SampleSink()
