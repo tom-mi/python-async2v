395e24fd01a260da081e48c499d3dc76b83f8226
Thomas Reifenberger <tom-mi@users.noreply.github.com>
2018-03-18 15:18:27 +0100

    Refactor cli part 1

--- async2v/cli.py
+++ async2v/cli.py
@@ -1,6 +1,7 @@
 import argparse
 
 import time
+from typing import List, Dict
 
 import logwood
 import sys
@@ -10,47 +11,59 @@ from async2v.application.graph import ApplicationGraph
 import async2v.application.graph
 
 
-class ApplicationLauncher:
+class Command:
 
-    def __init__(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-v', '--verbose', action='count', default=0)
-        parser.add_argument('-q', '--quiet', action='count', default=0)
-
-        self.setup_application_arguments(parser)
-        subparsers = parser.add_subparsers(help='command', dest='command')
-
-        run_parser = subparsers.add_parser('run')
-
-        graph_parser = subparsers.add_parser('graph')
-        graph_parser.add_argument('--source', help='Print dot code instead of creating graph', action='store_true')
-        graph_parser.add_argument('-o', '--output', metavar='FILENAME', default='graph',
-                                  help='Dot source output filename. '
-                                       'The source will be rendered to FILENAME.EXT according to output format.')
-        graph_parser.add_argument('-f', '--format', metavar='FORMAT', default='pdf',
-                                  choices=async2v.application.graph.get_formats(),
-                                  help='Output format')
-
-        self._parser = parser
-
-    def setup_application_arguments(self, parser: argparse.ArgumentParser):
+    @property
+    def name(self) -> str:
         raise NotImplementedError
 
-    def __call__(self):
-        args = self._parser.parse_args()
-        logwood.basic_config(
-            format='%(timestamp).6f %(level)-5s %(name)s: %(message)s',
-            level=self._get_loglevel(args),
-        )
+    @property
+    def help(self) -> str:
+        raise NotImplementedError
 
-        app = Application()
-        self.register_application_components(args, app)
+    @property
+    def needs_app(self) -> bool:
+        raise NotImplementedError
 
-        if not args.command:
-            self._parser.print_usage()
-            sys.exit(1)
+    @staticmethod
+    def add_arguments(parser: argparse.ArgumentParser):
+        raise NotImplementedError
 
-        if args.command == 'run':
+    @staticmethod
+    def __call__(args, app: Application = None):
+        raise NotImplementedError
+
+
+class LauncherModule:
+
+    def add_app_arguments(self, parser: argparse.ArgumentParser) -> None:
+        raise NotImplementedError
+
+    @property
+    def commands(self) -> List[Command]:
+        raise NotImplementedError
+
+
+class DefaultLauncherModule(LauncherModule):
+
+    def add_app_arguments(self, parser: argparse.ArgumentParser) -> None:
+        pass
+
+    @property
+    def commands(self) -> List[Command]:
+        return [self.RunCommand(), self.GraphCommand()]
+
+    class RunCommand(Command):
+        name = 'run'
+        help = 'Run the application'
+        needs_app = True
+
+        @staticmethod
+        def add_arguments(parser: argparse.ArgumentParser):
+            pass
+
+        @staticmethod
+        def __call__(args, app: Application = None):
             app.start()
             while app.is_alive():
                 try:
@@ -58,12 +71,86 @@ class ApplicationLauncher:
                 except KeyboardInterrupt:
                     break
             app.stop()
-        elif args.command == 'graph':
+
+    class GraphCommand(Command):
+        name = 'graph'
+        help = 'Draw an application graph using graphviz'
+        needs_app = True
+
+        @staticmethod
+        def add_arguments(parser: argparse.ArgumentParser):
+            group = parser.add_argument_group('Graph')
+            group.add_argument('--source', help='Print dot code instead of creating graph', action='store_true')
+            group.add_argument('-o', '--output', metavar='FILENAME', default='graph',
+                               help='Dot source output filename. '
+                                    'The source will be rendered to FILENAME.EXT according to output format.')
+            group.add_argument('-f', '--format', metavar='FORMAT', default='pdf',
+                               choices=async2v.application.graph.get_formats(),
+                               help='Output format')
+
+        @staticmethod
+        def __call__(args, app: Application = None):
             graph = ApplicationGraph(app._registry)
             if args.source:
                 print(graph.source())
             else:
                 graph.draw(args.output, args.format)
+
+
+class ApplicationLauncher:
+
+    def __init__(self):
+        self._launchers = [DefaultLauncherModule()]  # type: List[LauncherModule]
+        self._commands = {}  # type: Dict[str, Command]
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument('-v', '--verbose', action='count', default=0)
+        self.parser.add_argument('-q', '--quiet', action='count', default=0)
+
+        self.subparsers = self.parser.add_subparsers(help='command', dest='command')  # type: argparse._SubParsersAction
+
+    def add_module(self, launcher: LauncherModule):
+        self._launchers.append(launcher)
+
+    def add_app_arguments(self, parser: argparse.ArgumentParser):
+        raise NotImplementedError
+
+    def register_application_components(self, args, app: Application):
+        raise NotImplementedError
+
+    def main(self, args=None):
+        self._configure_parser()
+        args = self.parser.parse_args(args)
+        logwood.basic_config(
+            format='%(timestamp).6f %(level)-5s %(name)s: %(message)s',
+            level=self._get_loglevel(args),
+        )
+
+        if not args.command:
+            self.parser.print_usage()
+            sys.exit(1)
+        else:
+            command = self._commands[args.command]
+            if command.needs_app:
+                app = Application()
+                self.register_application_components(args, app)
+                command.__call__(args, app)
+            else:
+                command.__call__(args)
+
+    def _configure_parser(self):
+        for launcher in self._launchers:
+            for command in launcher.commands:
+                cmd_parser = self.subparsers.add_parser(command.name,
+                                                        help=command.help)  # type: argparse.ArgumentParser
+                command.add_arguments(cmd_parser)
+                if command.needs_app:
+                    self._add_all_app_args(cmd_parser)
+                self._commands[command.name] = command
+
+    def _add_all_app_args(self, parser: argparse.ArgumentParser):
+        self.add_app_arguments(parser)
+        for launcher in self._launchers:
+            launcher.add_app_arguments(parser)
 
     @staticmethod
     def _get_loglevel(args):
@@ -78,6 +165,3 @@ class ApplicationLauncher:
             return logwood.INFO
         else:
             return logwood.DEBUG
-
-    def register_application_components(self, args, app: Application):
-        raise NotImplementedError
