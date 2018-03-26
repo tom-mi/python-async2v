87f59939757fc8e59ab2236dead3f17c22367d96
Thomas Reifenberger <tom-mi@users.noreply.github.com>
2018-03-26 11:36:38 +0200

    Allow to draw opencv grayscale images via pygame

--- async2v/components/pygame/util/opencv.py
+++ async2v/components/pygame/util/opencv.py
@@ -5,4 +5,5 @@ from async2v.components.opencv.video import Frame
 
 
 def opencv_to_pygame(frame: Frame) -> pygame.Surface:
-    return pygame.surfarray.make_surface(cv2.transpose(cv2.cvtColor(frame.image, cv2.COLOR_BGR2RGB)))
+    conversion = cv2.COLOR_GRAY2RGB if frame.channels == 1 else cv2.COLOR_BGR2RGB
+    return pygame.surfarray.make_surface(cv2.transpose(cv2.cvtColor(frame.image, conversion)))
