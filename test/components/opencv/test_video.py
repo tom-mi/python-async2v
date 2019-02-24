afe1077d3e03f931a15e1f1071c40856b61e80da
Thomas Reifenberger <tom-mi@users.noreply.github.com>
2019-02-24 15:00:46 +0100

    Add tests for video source & sink

---
+++ test/components/opencv/test_video.py
@@ -0,0 +1,36 @@
+from typing import List
+
+from async2v.components.base import EventDrivenComponent
+from async2v.components.opencv.video import Frame, SimpleDisplaySink
+from async2v.fields import Buffer
+
+
+def test_video_source(app, video_source):
+    sink = SampleSink()
+
+    app.register(video_source, sink)
+    app.start()
+    app.join(10)
+
+    assert len(sink.log) == 150
+    assert isinstance(sink.log[0], Frame)
+    assert sink.log[0].width == 1280
+    assert sink.log[0].height == 720
+    assert sink.log[0].channels == 3
+
+
+def test_simple_sink(app, video_source):
+    sink = SimpleDisplaySink('source')
+
+    app.register(video_source, sink)
+    app.start()
+    app.join(10)
+
+
+class SampleSink(EventDrivenComponent):
+    def __init__(self, key='source'):
+        self.input = Buffer(key, trigger=True)
+        self.log: List[Frame] = []
+
+    async def process(self):
+        self.log += self.input.values
--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,10 @@
+import os.path
+
 import logwood
 import pytest
 from logwood.handlers.stderr import ColoredStderrHandler
+
+from async2v.application import Application
 
 
 @pytest.fixture(autouse=True)
@@ -10,3 +14,20 @@ def configure_logging():
         handlers=[ColoredStderrHandler()],
         format='%(timestamp).6f %(level)-5s %(name)s: %(message)s',
     )
+
+
+@pytest.fixture
+def app(configure_logging):
+    return Application()
+
+
+@pytest.fixture
+def video_file():
+    return os.path.join(os.path.dirname(__file__), 'data', 'video.mp4')
+
+
+@pytest.fixture
+def video_source(video_file):
+    from async2v.components.opencv.video import VideoSource, VideoSourceConfig
+    source_config = VideoSourceConfig(path=video_file, fps=200)  # Pass a high frame rate to speed up test
+    return VideoSource(source_config, key='source')
