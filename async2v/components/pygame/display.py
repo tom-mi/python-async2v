8830a65568f5ea2ecafa1d5a10439f35e034a08a
Thomas Reifenberger <tom-mi@users.noreply.github.com>
2019-02-24 21:47:36 +0100

    Support different bit depths on pygame display
    - Convert pygame surface generated from opencv frames

--- async2v/components/pygame/display.py
+++ async2v/components/pygame/display.py
@@ -333,4 +333,4 @@ class OpenCvDebugDisplay(OpenCvMultiDisplay):
 
 def _opencv_to_pygame(frame: Frame) -> pygame.Surface:
     conversion = cv2.COLOR_GRAY2RGB if frame.channels == 1 else cv2.COLOR_BGR2RGB
-    return pygame.surfarray.make_surface(cv2.transpose(cv2.cvtColor(frame.image, conversion)))
+    return pygame.surfarray.make_surface(cv2.transpose(cv2.cvtColor(frame.image, conversion))).convert()
